16de16d7d828b54fd56f1ce3b2f268b35d1e9cce
Lim Seung Hyuk <LimSH-98>
2022-05-04 23:26:56 +0900

    즉시 낙찰 구현, currentBid -> nextBid로 필드명 변경, 낙찰 상품 버튼 비활성화

--- src/main/java/jpabook/jpashop/domain/Post.java
+++ src/main/java/jpabook/jpashop/domain/Post.java
@@ -24,7 +24,7 @@ public class Post {
     private int startBid; // 시작가
     private int winningBid; // 낙찰가 -> 1000, 5000, 10000, 15000, 20000, 50000
     private int unitBid; // 입찰 단위
-    private int currentBid; // 현재 입찰가
+    private int nextBid; // 현재 입찰가
     private Timestamp regisTime; // 경매 올린 시간 -> yyyy-mm-dd hh:mm:ss
     private int auctionPeriod; // 경매 기간 -> 12시간, 24시간, 36시간, 48시간
     private String status; // 상품 상태 -> 낙찰 중, 낙찰 완료
