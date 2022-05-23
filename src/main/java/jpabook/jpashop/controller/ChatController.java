6047a22c778360db7b5cd09fb5b5bfed45c89d01
Lim Seung Hyuk <LimSH-98>
2022-05-23 19:26:18 +0900

    메시지 수정 중

--- src/main/java/jpabook/jpashop/controller/ChatController.java
+++ src/main/java/jpabook/jpashop/controller/ChatController.java
@@ -1,16 +1,15 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.Form.PostForm;
 import jpabook.jpashop.domain.ChatMessage;
-import jpabook.jpashop.domain.ChatRoom;
 import jpabook.jpashop.service.ChatMessageService;
-import jpabook.jpashop.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
+import netscape.javascript.JSObject;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,9 +27,24 @@ public class ChatController {
 
     @MessageMapping(value = "/chat/message")
     public void message(ChatMessage message) throws Exception {
-        message.setMessage(message.getMessage());
+        // message.setMessage(message.getMessage());
         // template.convertAndSend("/sub/chat/room/" + message.getChRoomId(), message);
         template.convertAndSend("/sub/chat/room", message);
         chatMessageService.saveChatMessage(message);
+    }
+
+    @MessageMapping(value = "/item/bid")
+    public void auction(Map params) throws Exception {
+        template.convertAndSend("/sub/item/bid", params);
+    }
+
+    @MessageMapping(value = "/item/buy")
+    public void buy(PostForm post) throws Exception {
+        template.convertAndSend("/sub/item/buy", post);
+    }
+
+    @MessageMapping(value = "/item/expired")
+    public void itemExpired(PostForm post) throws Exception {
+        template.convertAndSend("/sub/item/expired", post);
     }
 }
