ca7c948462852222bb7771203f3561f26318c554
Lim Seung Hyuk <LimSH-98>
2022-06-01 22:47:15 +0900

    sendSms 인자 수정 등

--- src/main/java/jpabook/jpashop/domain/PostUser.java
+++ src/main/java/jpabook/jpashop/domain/PostUser.java
@@ -15,12 +15,12 @@ public class PostUser {
     @Column(name="postuser_id")
     private Long id;
     private Long postId; // 상품 등록한 사용자 id
-    private String bidUserAccountId;
+    private String bidUserAccountId; // 입찰한 사용자의 계정 id
     private String bidUserName; //입찰한 사용자의 닉네임
     private String postUserAccountId;
     private String postUserName; //경매글 등록자 닉네임
-    private int bid;
-    private Date bidDate;
+    private int bid; // 입찰 금액
+    private Date bidDate; // 입찰한 날짜 및 시간
     private String type; // 입찰, 낙찰, 즉시 구매 인지.
 
 }
